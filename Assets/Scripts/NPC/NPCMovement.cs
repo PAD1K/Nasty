01baa90bc245c0ba37f93f8c4136be890eac513c
Aramymo <maxoff9617@gmail.com>
2025-01-19 14:06:22 +0300

    Added customizable speed and navmeshplus

--- Assets/Scripts/NPC/NPCMovement.cs
+++ Assets/Scripts/NPC/NPCMovement.cs
@@ -3,9 +3,10 @@ using UnityEngine;
 public class NPCMovement : MonoBehaviour, IMovable
 {
     [SerializeField] private Rigidbody2D _npcRb;
+    [SerializeField] private float _movementSpeedMultiplier = 1;
 
     public void Move(Vector2 direction)
     {
-        _npcRb.AddForce(direction*2, ForceMode2D.Force);
+        _npcRb.AddForce(direction * _movementSpeedMultiplier, ForceMode2D.Force);
     }
 }
